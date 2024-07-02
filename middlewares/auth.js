c0b1c53adb86f4452946a2954fde7e19096fcea8
Aul-rhmn <113172027+Aul-rhmn@users.noreply.github.com>
2024-07-02 15:34:26 +0700

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,10 +2,9 @@ const isLogin = (req, res, next) => {
     if(req.session.user == null || req.session.user == undefined) {
         req.flash("alertMessage", "Session expired. Please log in again.");
         req.flash("alertStatus", "danger");
-        
+        return res.redirect('/admin/signin');
     }else {
         next()
-        return res.redirect('/admin/signin');
     }
 }
 
